3e69bee3c22bfd4f430e478ca83e4c87651a5ab1
XenoAmess <xenoamess@gmail.com>
2026-09-16 00:48:24 +0800

    Bound-multistage-browser-acceptance-runtime

--- coat_of_arms_editer_of_ck3/e2e/standalone-image-fit.spec.ts
+++ coat_of_arms_editer_of_ck3/e2e/standalone-image-fit.spec.ts
@@ -38,6 +38,7 @@ function bgraDds(
 }
 
 test('fits an uploaded image without CK3, MCP, or Java', async ({ page }) => {
+  test.setTimeout(180_000)
   const apiRequests: string[] = []
   page.on('request', (request) => {
     if (new URL(request.url()).pathname.startsWith('/api/')) apiRequests.push(request.url())
